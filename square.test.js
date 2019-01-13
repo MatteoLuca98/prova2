5be751d07c1142202bf0f6340338744c699a165d
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-13 18:25:13 +0100

    modify

--- square.test.js
+++ square.test.js
@@ -1,13 +1,13 @@
-const square = require('./square');
+const square = require('./square')
 
 test('square(1) should return -1', () => {
     expected(square(1)).toBe(-1)
-});
+})
 
 test('square ("abcd") shoud return 16', () => {
     expected(square('ciao').toBe(16));
-});
+})
 
 test('square(null) shoud return -1', () => {
     expected(square(null).toBe(-1))
-});
+})
